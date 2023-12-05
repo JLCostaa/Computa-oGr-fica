ef0c51748a3ca0d6da4c9ee689999b4d09ed74ab
JlCosta <Jullia.assis@estudante.ifgoiano.edu.br>
2023-12-05 14:21:10 -0300

    Create Ponto.java

---
+++ Ponto.java
@@ -0,0 +1,16 @@
+// Source code is decompiled from a .class file using FernFlower decompiler.
+class Ponto {
+   double x;
+   double y;
+
+   public Ponto() {
+    x = 0;
+    y = 0;
+}
+
+
+   public Ponto(double var1, double var3) {
+      this.x = var1;
+      this.y = var3;
+   }
+}
